0c95ccec9b2de509f50684ba7c8d24c5d8c31af2
AshayOfficial <ashay.gawane@neosoftmail.com>
2023-08-02 10:45:48 +0530

    Changes in code added git ignore file

--- Que_01_08_23.java
+++ Que_01_08_23.java
@@ -2,28 +2,46 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Que010823 {
+public class Que_01_08_23 {
 
+//    Given a list of tasks with deadlines and total profit earned on completing a task, find the maximum profit earned
+//    by executing the tasks within the specified deadlines. Assume that each task takes one unit of time to complete,
+//    and a task can’t execute beyond its deadline. Also, only a single task will be executed at a time.
+//
+//    For example, consider the following set of tasks with a deadline and the profit associated with it. If we choose
+//    tasks 1, 3, 4, 5, 6, 7, 8, and 9, we can achieve a maximum profit of 109. Note that task 2 and task 10 are left
+//    out.
+//    Tasks	Deadlines	  Profit
+//      1	    9	        15
+//      2	    2	        2
+//      3	    5	        18
+//      4	    7	        1
+//      5	    4	        25
+//      6	    2	        20
+//      7	    5	        8
+//      8	    7	        10
+//      9	    4	        12
+//      10	    3	        5
     public static void maxProfitDeadline(List<DeadlineProfitTask> deadlineProfitTasks) {
         int maxProfit = 0;
         int maxDeadline = Integer.MIN_VALUE;
-        for (DeadlineProfitTask deadlineProfitTask: deadlineProfitTasks) {
+        for (DeadlineProfitTask deadlineProfitTask: deadlineProfitTasks) {      //max deadline value
             if (deadlineProfitTask.deadline > maxDeadline)
                 maxDeadline = deadlineProfitTask.deadline;
         }
 
         int[] tasksList = new int[maxDeadline];
         Collections.sort(deadlineProfitTasks);
-        for (int i = 0; i <= tasksList.length; i++) {
+        for (int i = 0; i < deadlineProfitTasks.size(); i++) {
             DeadlineProfitTask deadlineProfitTask = deadlineProfitTasks.get(i);
             if (tasksList[deadlineProfitTask.deadline - 1] == 0) {
-                tasksList[deadlineProfitTask.deadline - 1] = deadlineProfitTask.tasks;
-                maxProfit = deadlineProfitTask.profit+maxProfit;
+//                tasksList[deadlineProfitTask.deadline - 1] = deadlineProfitTask.tasks;
+                maxProfit = deadlineProfitTask.profit + maxProfit;
             }
             else if (tasksList[deadlineProfitTask.deadline - 1] != 0) {
-                for (int j = deadlineProfitTask.deadline - 1; j >= 0 ; j--) {
+                for (int j = deadlineProfitTask.deadline - 2; j >= 0 ; j--) {
                     if (tasksList[j] == 0) {
-                        tasksList[j] = deadlineProfitTask.tasks;
+//                        tasksList[j] = deadlineProfitTask.tasks;
                         maxProfit = deadlineProfitTask.profit+maxProfit;
                         break;
                     }
@@ -46,7 +64,7 @@ public class Que010823 {
         deadlineProfitTasks.add(new DeadlineProfitTask(7, 8, 10));
         deadlineProfitTasks.add(new DeadlineProfitTask(4, 9, 12));
         deadlineProfitTasks.add(new DeadlineProfitTask(3, 10, 5));
-        Que010823.maxProfitDeadline(deadlineProfitTasks);
+        Que_01_08_23.maxProfitDeadline(deadlineProfitTasks);
     }
 
     static class DeadlineProfitTask implements Comparable<DeadlineProfitTask> {
@@ -63,15 +81,6 @@ public class Que010823 {
         @Override
         public int compareTo(DeadlineProfitTask deadlineProfitTask) {
             return deadlineProfitTask.profit - this.profit;
-        }
-
-        @Override
-        public String toString() {
-            return "DeadlineProfitTask{" +
-                    "deadline=" + deadline +
-                    ", tasks=" + tasks +
-                    ", profit=" + profit +
-                    '}';
         }
     }
 }
